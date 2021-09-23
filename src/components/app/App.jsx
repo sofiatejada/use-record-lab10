8a539db4d462a9c4cfc4d060921b5fef8a7080a6
Sofia Tejada <sofianais@icloud.com>
2021-09-22 19:51:31 -0700

    getting close

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,43 +1,40 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import React from "react";
 
 const useRecord = (init) => {
 
   const [current, setCurrent] = useState(init);
   const [colorInputs, setPastColors] = useState([init]);
-  
 
-  const undo = () => {
-      const arrayLength = colorInputs.length;
-
-      setCurrent(colorInputs[arrayLength - 1])
+  const redo = () => {
+    const currentIndex = colorInputs.indexOf(current);
+    setCurrent(colorInputs[currentIndex + 1]);
   }
-
-  // const redo = () => {
-
-  // }
 
   const record = (value) => {
-    colorInputs.push(value);
+    const currentIndex = colorInputs.indexOf(current);
+    colorInputs.splice(currentIndex, 0, value);
     setPastColors(colorInputs);
     setCurrent(value)
+    console.log(currentIndex);
+  }
+
+  const undo = () => {
+    const currentIndex = colorInputs.indexOf(current);
+    setCurrent(colorInputs[currentIndex - 1]);
   }
   console.log(colorInputs);
-  
-  return { current, record, undo };
 
-  
-
+  return { current, record, undo, redo };
 };
 
 function App() {
-  const { current, record, undo } = useRecord('#FF0000');
-  console.log(current);
+  const { current, record, undo, redo } = useRecord('#FF0000');
 
   return (
     <>
-      <button onClick={console.log('undo handler here')}>undo</button>
-      <button onClick={console.log('redo handler here')}>redo</button>
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
       <input type="color" value={current} onChange={({ target }) => record(target.value)} />
       <div style={{ backgroundColor: current, width: '10rem', height: '10rem' }}></div>
     </>
